cad075239f09962ee660625c3d28173fb40228a0
FranseFrikandel <crgamingserver@hotmail.com>
2021-10-14 17:31:47 +0200

    CoG toegevoegd

--- init.py
+++ init.py
@@ -1,11 +1,15 @@
 
 
 if __name__ == "__main__":
+    """
+    Hier voeren we tests uit. Door bekende waardes te nemen en ze ook te laten 
+    berekenen door de library weten we of de library deze waardes correct berekend
+    """
     errMargin = 1.005 # Foutmarge van 0,5% vanwege float rounding errors
 
     import shipCalculator as sc
     
-    vorm = sc.Shape("rect", 50, 50, 10, 0, 0)
+    vorm = sc.Shape("rect", 50, 50, 10, 0, 0, 0)
     ship = sc.Ship()
     ship.addShape(vorm)
 
--- shipCalculator.py
+++ shipCalculator.py
@@ -1,14 +1,15 @@
 import numpy as np
 from dataclasses import dataclass
 
-from numpy.core.numeric import moveaxis
-
 @dataclass
 class Shape():
     """
     Een losse vorm. Meerdere vormen kunnen samen een schip vormen die bestaat uit meerdere simplistische vormen.
 
+    Deze vormen zijn massaloos, zie de CoG class voor zwaartepunten/gewichten toe te voegen.
+
     XY coordinaten zijn t.o.v. centroid. Voor een rechthoek dus het middelpunt, voor een driehoek op 1/3h, etc.
+    Z coordinaten zijn t.o.v. de onderkant. Z=0 is de onderkant van het schip.
 
     X is lengte van het schip
     Y is de breedte van het schip
@@ -17,7 +18,7 @@ class Shape():
 
     Ondersteunde vormen:
     rect - Rechthoek.
-    circle - Cirkel. Hierbij voor lengte en breedte dezelfde waarde ingeven. TODO: Vervangen met ellips
+    circle - Cirkel. Hierbij voor lengte en breedte dezelfde waarde ingeven. TODO: Vervangen met ellips?
     triangle - Driehoek. WIP
     """
     sType: str
@@ -26,6 +27,7 @@ class Shape():
     height: float
     x: float
     y: float
+    z: float
 
     def __post_init__(self):
         if self.sType == "circle":
@@ -64,15 +66,59 @@ class Shape():
             return moi + steiner
 
 
+@dataclass
+class CoG():
+    """
+    Voegt zwaartepunten toe aan een schip.
+
+    Gewicht ik kg
+    """
+    weight: float
+    x: float
+    y: float
+    z: float
+
+
+@dataclass
+class LiqCont():
+    """Een vloeistofcontainer. Nog niet geimplementeerd."""
+    length: float
+    width: float
+    height: float
+
+
 class Ship():
+    """
+    Een schip-class. Bewaard alle variabelen en berekent vanalles.
+    """
     def __init__(self, **kwargs):
+        """
+        kwargs:
+        diepgang
+        rho
+        GM
+        # TODO: Welke variabelen hebben we nog nodig?
+        """
         self.shapes = []
+        self.CoGs = []
+        if "diepgang" in kwargs:
+            self.T = kwargs["diepgang"]
+        if "rho" in kwargs:
+            self.rho = kwargs["rho"]
+        else:
+            self.rho = 1025 # kg/m^2
 
     def addShape(self, shapeOb):
         if not isinstance(shapeOb, Shape):
             raise TypeError("Not a shape object")
         
         self.shapes.append(shapeOb)
+
+    def addCoG(self, CoGOb):
+        if not isinstance(CoGOb, CoG):
+            raise TypeError("Not a CoG object")
+        
+        self.shapes.append(CoGOb)
     
     def getArea(self):
         a = 0
@@ -91,3 +137,31 @@ class Ship():
         for sh in self.shapes:
             m += sh.getyMOI()
         return m
+    
+    def getMass(self):
+        mt = 0
+        for m in self.CoGs:
+            mt += m.weight
+        return mt
+
+    def getCoG(self):
+        """
+        Berekent het zwaartepunt en geeft het terug als een X,Y,Z tuple
+        """
+        mt = 0
+        ax = 0
+        ay = 0
+        az = 0
+        for m in self.CoGs:
+            mt += m.weight
+            ax += m.weight * m.x
+            ay += m.weight * m.y
+            az += m.weight * m.z
+        x = ax / mt
+        y = ax / mt
+        z = ax / mt
+        return (x,y,z)
+    
+    def getTPC(self):
+        # TODO?
+        return rho / (100000 * self.getArea())
